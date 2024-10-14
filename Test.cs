ec3693969410cdf6a630d53d8cfce51617485862
jlim8904 <92771562+jlim8904@users.noreply.github.com>
2024-10-14 16:40:00 +0800

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,46 @@
+class Test {
+  public test1() {
+    const a = 1;
+    
+    const b = "{\"";
+
+    const c = 1; \\ {{{{}{{{{}{}}}{{{{
+
+    const d = 1;
+  }
+  
+  public test2() {
+    /*
+    {{{{{}}{{{{{{}}}}}}}}}}}}}{{{{{{{{{}}{
+    */
+    const a = 1;
+    
+    const b = `}"`;
+
+    const c = '{';
+
+    const d = 1;
+  }
+  
+  public test3() {
+    const a = 1;
+    
+    const b = `}"`;
+
+    const c = "fdgcfd\
+      \"dsadasd{{}\
+    asdasdasd";
+
+    const d = 1;
+  }
+  
+  public test4() {
+    const a = 1;
+    
+    const b;
+
+    const c = '{';
+
+    const d = 1;
+  }
+}
